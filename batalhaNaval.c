c127f6159c9a1151d0ea8a14c8b93cefc3247743
liosnake <juliocezarrsouza@gmail.com>
2025-09-08 14:18:28 +0000

    2 navios na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 int main (){
 
@@ -19,25 +19,31 @@ int main (){
     };
 
     printf("Bem vindo ao jogo batalha naval! \n");
-    printf("O tabuleiro é 10x10, e você tem 10 chances de acertar o navio inimigo! \n");
-    printf("O tabuleiro é representado por ~, e o navio por 3. \n");
+    printf("O tabuleiro é 10x10 \n");
+    printf("O tabuleiro é representado por 0, e o navio por 3. \n");
     printf("Boa sorte! \n");
 
     tabuleiro[1][7] = '3';
     tabuleiro[1][8] = '3';
     tabuleiro[1][9] = '3';
-    tabuleiro[2][2] = '3';
-    tabuleiro[3][2] = '3';  
-    tabuleiro[4][2] = '3';
+    tabuleiro[2][1] = '3';
+    tabuleiro[3][1] = '3';  
+    tabuleiro[4][1] = '3';
+    tabuleiro[9][0] = '3';
+    tabuleiro[8][1] = '3';
+    tabuleiro[7][2] = '3';
+    tabuleiro[9][3] = '3';
+    tabuleiro[8][4] = '3';
+    tabuleiro[7][5] = '3';
 
 
 
     for(int i=0; i<1; i++){
-        printf("  A B C D E F G H I J\n");
-        for(int j=0; j<10; j++){
-            printf("%d ", j);
-            for(int k=0; k<10; k++){
-                printf("%c ", tabuleiro[j][k]);
+        printf("  0 1 2 3 4 5 6 7 8 9\n");
+        for(int i=0; i<10; i++){
+            printf("%d ", i);
+            for(int j=0; j<10; j++){
+                printf("%c ", tabuleiro[i][j]);
             }
             printf("\n");
         }
